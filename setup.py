e816dbcc45d5205383ef51f00aacebfde4ff72d9
Jeremy Lewis <luislew@gmail.com>
2018-05-16 15:15:50 -0700

    Switch to including LICENSE/VERSION via MANIFEST.in

--- setup.py
+++ setup.py
@@ -55,10 +55,6 @@ setup(
     license='MIT License',
     packages=find_packages('src'),
     package_dir={'': 'src'},
-    package_data={'': [
-        '../LICENSE',
-        '../VERSION',
-    ]},
     install_requires=install_requires,
     extras_require={
         'dev': dev_requires,
